7afdee118fc62cb93071c4b07ba774a29eedd670
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-09-21 11:57:17 -0400

    added a user input and an add button on the front page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Todos from '../../containers/Todos';
+import usePostTodo from '../../Hooks/usePostTodo';
 
 export default function App() {
+
   return <>
     <Todos />
+
   </>;
 }
--- src/service/todoAppFuncs.js
+++ src/service/todoAppFuncs.js
@@ -1,16 +1,27 @@
 // const URL = 'https://jsonplaceholder.typicode.com/posts?userId=1';
 const URL = 'http://localhost:7890/api/v1/todos';
 
-export const getTodosFunc = () => {    
+export const getTodosFunc = () => {
   return fetch(URL)
     .then((response) => response.json())
-    .then(
-      (todos) => todos.map(
-              (todo) => ({
-          id: todo.id,
-          title: todo.description,
-          completed: todo.isCompleted,
-        }))
+    .then((todos) =>
+      todos.map((todo) => ({
+        id: todo.id,
+        title: todo.description,
+        completed: todo.isCompleted,
+      }))
     );
 };
 
+export const postOneTodo = (description, isCompeleted) => {
+  return fetch(URL, {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json', 
+    }, 
+    body: JSON.stringify({description, isCompeleted})
+  }) 
+    .then(res => res.json()) 
+    .catch(err => console.log(err))
+}
